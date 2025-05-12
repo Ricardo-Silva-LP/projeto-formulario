d7e0f1674c7023beb5dbe3a277943fc9fcf97661
Ricardo Silva <ricardao.lp23@gmail.com>
2025-05-12 09:14:26 -0400

    Feito Alterações no JS

--- src/js/validacao.js
+++ src/js/validacao.js
@@ -1,38 +1,3 @@
-
-// document.getElementById("form").addEventListener("submit", function (e) {
-//     e.preventDefault();
-
-//     const inputs = document.querySelectorAll(".formulario input");
-//     const textarea = document.querySelector(".mensagem");
-//     const mensagensErro = document.querySelectorAll(".formulario a");
-
-//     let valido = true;
-
-//     inputs.forEach((input, index) => {
-//         if (input.value.trim() === "") {
-//             input.style.border = "2px solid red"
-//             mensagensErro[index].style.display = "block";
-//             valido = false;
-//         } else {
-//             input.style.border = "2px solid green";
-//             mensagensErro[index].style.display = "none";
-//         }
-//     });
-
-
-//     const msgIndex = inputs.length;
-//     if (textarea.value.trim() === "") {
-//         textarea.style.border = "2px solid red";
-//         mensagensErro[msgIndex].style.display = "block";
-//         valido = false;
-//     } else {
-//         textarea.style.border = "2px solid green";
-//         mensagensErro[msgIndex].style.display = "none";
-//     }
-
-
-// });
-
 document.getElementById("form").addEventListener("submit", function (e) {
     e.preventDefault();
 
